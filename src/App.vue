ce77d76afec30927574fed4fd477e1fc52b90e72
Queenie <kunwen0708@hotmail.com>
2018-11-08 21:13:31 +0800

    follow youtube with test.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Die ganze Welt ist eine "/>
+    <Test/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/test'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,Test
   }
 }
 </script>
---
+++ src/components/test.vue
@@ -0,0 +1,61 @@
+<template>
+ <div class="test">
+     <input type="text" v-model="title"> <br />
+     <h1>{{title}}</h1>
+     <p>{{user.firstName}}</p>
+     <p v-text="user.firstName"></p>
+
+     <ul>
+         <li v-for="item in items"> {{ item.title }} </li>
+     </ul>
+     <button v-on:click="greet('Queenie')">Hello</button>
+     <br/>
+     <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="hitEnter">
+     <br/>
+     <label>First Name</label><input type="text" v-model="user.firstName">
+     <br/>
+     <label>First Name</label><input type="text" v-model="user.lastName">
+     <h3>{{fullName}}</h3>
+ </div>
+</template>
+
+<script>
+  export default{
+      name:'test',
+      data(){
+          return{
+              title:"Hello World",
+              user:{
+                  firstName:"Queenie",
+                  lastName:"Kun"
+              },
+              items: [
+                  { title: "item One" },
+                  {title:"item Two"},
+                  {title:"Item Three"}
+              ]
+          }
+      },
+      methods:{
+          greet: function(hi) {
+              alert(hi);
+          },
+          pressKey: function(e) {
+              console.log(e.target.value);   
+          },
+          hitEnter: function() {
+              console.log('you hit enter');
+              
+          }
+      },
+      computed:{
+          fullName: function() {
+              return this.user.firstName + ' ' + this.user.lastName
+          }
+      }
+
+  }
+</script>
+
+<style>
+</style>
